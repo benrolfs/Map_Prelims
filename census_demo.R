e8a665e48c0ceea604d7629bffdf6ae762b71c93
benrolfs <benrolfs@rolfsmac.local>
2013-09-17 20:50:04 -0700

    1st sync

---
+++ census_demo.R
@@ -0,0 +1,6 @@
+# Here we will explore the "UScensus20xx" packages. There is a good overview
+# of these packages here: http://www.jstatsoft.org/v37/i06/paper
+
+library(UScensus2010)
+# install shape files using the following: install.cdp("osx")
+library(UScensus2010cdp)
